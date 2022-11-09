fb857345193a6d1018705145bca7c53fab73d039
Michelle Chen <chensiqi96@gmail.com>
2022-11-08 20:50:48 -0800

    tip calc and magic 8 ball express app created

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get("/greeting/:name", (req,res) => {
 
 })
 
+//Tip Calculator
 
 app.get("/tip/:total/:tipPercentage", (req, res) => {
 
@@ -20,10 +21,31 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
 })
 
 
+//Magic 8 Ball
+
+app.get("/magic/:question", (req,res) => {
+const questionAsked = req.params.question.charAt(0).toUpperCase() + req.params.question.slice(1).toLowerCase()
+
+const quotes = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+const randomNumber = Math.round(Math.random()*quotes.length)
+const randomQuote = quotes[randomNumber]
+
+    res.send(`<h1>Your question: ${questionAsked}?</h1> <h1>My answer: ${randomQuote}</h1>`)
+
+})
 
 
+// app.get("/count/:beverage", (req, res) => {
 
+//     const beverage = req.params.beverage
+//     const numberOfBev = req.query.numberOfBev
 
+//     for (i = numberOfBev; i>0; i-=1){
+//     setTimeout(() => { res.send(`You have ${i} of ${beverage} left.`)
+// }, 1000)
+// }}
+
+// )
 
 
 
